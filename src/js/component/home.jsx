79099ec99644e727cb224aed4f0858e75c9a5851
DOSJ3 <danielstjuste@gmail.com>
2022-11-10 00:47:58 +0000

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -13,9 +13,9 @@ const Home = () => {
         <div className="text-center">
             <div className="pole"></div>
             <div className="traffic-light">
-                <div onClick={()=>setSelectedColor("red")} className={"light red" +(selectedColor === "red" ? "glowred" : "" )}></div>
-                <div onClick={()=>setSelectedColor("yellow")} className={"light yellow" +(selectedColor === "yellow" ? "glowyellow" : "" )}></div>
-                <div onClick={()=>setSelectedColor("green")} className={"light green" +(selectedColor === "green" ? "glowgreen" : "" )}></div>
+                <div onClick={()=>setSelectedColor("red")} className={"light red " +(selectedColor === "red" ? "glowred" : "" )}></div>
+                <div onClick={()=>setSelectedColor("yellow")} className={"light yellow " +(selectedColor === "yellow" ? "glowyellow" : "" )}></div>
+                <div onClick={()=>setSelectedColor("green")} className={"light green " +(selectedColor === "green" ? "glowgreen" : "" )}></div>
             </div>
         </div>
     )
